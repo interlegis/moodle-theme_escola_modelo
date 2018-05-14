3b6b1a325822ee8ce050fee20d3bf540fbce9a2e
Matheus Garcia <garcia.figueiredo@gmail.com>
2018-05-14 17:26:26 -0300

    Início do desenvolvimento do tema Escola Modelo

---
+++ lang/en/theme_escola_modelo.php
@@ -0,0 +1,18 @@
+<?php
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+ 
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();                                                                                                
+ 
+// A description shown in the admin theme selector.                                                                                 
+$string['choosereadme'] = 'Theme photo is a child theme of Boost. It adds the ability to upload background photos.';                
+// The name of our plugin.                                                                                                          
+$string['pluginname'] = 'Photo';                                                                                                    
+// We need to include a lang string for each block region.                                                                          
+$string['region-side-pre'] = 'Right';
+
+
+// Background image for login page.                                                                                                 
+$string['loginbackgroundimage'] = 'Login page background image';                                                                    
+// Background image for login page.                                                                                                 
+$string['loginbackgroundimage_desc'] = 'An image that will be stretched to fill the background of the login page.';
