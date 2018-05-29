a05260fa77eff6a460fbbc17cfde2781608501ba
eduardo <eduardocserra@outlook.com>
2018-05-29 19:45:16 -0300

    Recriação do tema escola_modelo e teste com bd

--- settings.php
+++ settings.php
@@ -92,18 +92,5 @@ if ($ADMIN->fulltree) {
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
 
-
-    // Login page background setting.                                                                                               
-    // We use variables for readability.                                                                                            
-    $name = 'theme_escola_modelo/loginbackgroundimage';                                                                                     
-    $title = get_string('loginbackgroundimage', 'theme_photo');                                                                     
-    $description = get_string('loginbackgroundimage_desc', 'theme_photo');                                                          
-    // This creates the new setting.                                                                                                
-    $setting = new admin_setting_configstoredfile($name, $title, $description, 'loginbackgroundimage');                             
-    // This means that theme caches will automatically be cleared when this setting is changed.                                     
-    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
-    // We always have to add the setting to a page for it to have any effect.                                                       
-    $page->add($setting);
-
     $settings->add($page);
 }
